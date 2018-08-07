cecd1958fcbd4d54657a43d793707685c657df53
Richard Gurney <richgurney1987@gmail.com>
2018-08-07 23:04:24 +0100

    added test for all the calculator functions

--- spec/advancedCalcSpec.js
+++ spec/advancedCalcSpec.js
@@ -1,32 +1,37 @@
 // This is the container for the testing what file are you testing
-describe("Testing the advancedCalc.js with TDD",function() {
+describe("Testing the calculatorMethods.js with TDD",function() {
     // Include the calc code into the testing scope
     var app = require('../lib/calculatorMethods');
 
-    // What is the function that we are testing
-    describe("Testing the multiplyTwoNumbers() function", function(){
+    // What should the function do?
+    // multiplyTwoNumbers
+    it("should multiply two number together", function(){
+      // Assertion to check the function is correct
+      // Called Expectations and Matchers (Which a boolean)
+      expect(app.multiplyTwoNumbers(3,2)).toEqual(6);
+    });
 
-      // What should the function do?
-      it("should multiply two number together", function(){
+    // What should the function do?
+    // addTwoNumbers
+    it("should add two numbers together", function(){
+      // Assertion to check the function is correct
+      // Called Expectations and Matchers (Which a boolean)
+      expect(app.addTwoNumbers(3,2)).toEqual(5);
+    });
 
-        // Assertion to check the function is correct
-        // Called Expectations and Matchers (Which a boolean)
-        expect(app.multiplyTwoNumbers(3,2)).toEqual(6);
+    // What should the function do?
+    // minusTwoNumbers
+    it("should subtract two numbers", function(){
+      // Assertion to check the function is correct
+      // Called Expectations and Matchers (Which a boolean)
+      expect(app.minusTwoNumbers(3,2)).toEqual(1);
+    });
 
-      });
-    })
-
-    // What is the function that we are testing
-    describe("Testing the addTwoNumbers() function", function(){
-
-      // What should the function do?
-      it("should add two numbers together", function(){
-
-        // Assertion to check the function is correct
-        // Called Expectations and Matchers (Which a boolean)
-        expect(app.addTwoNumbers(3,2)).toEqual(5);
-
-      });
-    })
-
+    // What should the function do?
+    // divideTwoNumbers
+    it("should divide two numbers", function(){
+      // Assertion to check the function is correct
+      // Called Expectations and Matchers (Which a boolean)
+      expect(app.divideTwoNumbers(4,2)).toEqual(2);
+    });
 });
